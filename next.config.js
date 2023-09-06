5693ed4b0e9942222d171af08172a7b5f17c24ec
Robert McLean <robert.mclean@ncino.com>
2023-09-06 13:48:28 -0400

    Add output to config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+	assetPrefix: assetPrefix,
+	basePath: basePath,
+	output: "export",
     webpack(config) {
         config.resolve.extensionAlias = {
             ".js": [".js", ".ts"],
